1a62dcff36a47f50ea0d05b5523a2a14b160193c
crawlic-stud <nikitosik0726@gmail.com>
2021-10-23 14:31:45 +0300

    GUI improvement

--- main.py
+++ main.py
@@ -2,8 +2,9 @@ import random
 from tkinter import Tk, simpledialog
 import pygame
 from classes import Game, Button
-from utils import closest_divs
+from utils import closest_divs, print_text, render_text
 from math import sqrt
+pygame.font.init()
 
 
 class PercentVisualisation(Game):
@@ -16,6 +17,7 @@ class PercentVisualisation(Game):
 
     def __init__(self):
         super().__init__((800, 800), 60, caption='Percent Visualisation', first_state=self.main)
+
         self.user_input = 1
         self.grid = []
         self.max_grid_size = round(1 / self.user_input * 100)
@@ -23,20 +25,44 @@ class PercentVisualisation(Game):
         self.square_height = self.height - 100
         self.man_size = int(sqrt((self.square_width * self.square_height) / self.max_grid_size))
         self.random_pos = (0, 0)
+        self.info = False
+
+        self.buttons = [
+            Button([10, self.height-40, 200, 30], self.screen, 'Change value',
+                   (50, 50, 50), self.BACKGROUND, command=self.user_entry),
+            Button([210, self.height - 40, 200, 30], self.screen, 'Random value',
+                   (50, 50, 50), self.BACKGROUND, command=self.random_value),
+            Button([410, self.height - 40, 200, 30], self.screen, 'Show info',
+                   (50, 50, 50), self.BACKGROUND, command=self.info_switch),
+            Button([610, self.height - 40, 180, 30], self.screen, 'Options',
+                   (50, 50, 50), self.BACKGROUND, command=None),
+        ]
 
     def setup(self):
         """Setup before starting program loop."""
         super().setup()
-        # self.user_entry()
+        self.update_grid()
 
     def user_entry(self):
         """Enter new percent value."""
         root = Tk()
         root.withdraw()
+        user_input = self.user_input
         self.user_input = simpledialog.askfloat('Entry', 'Enter your percent value here:',
                                                 minvalue=0.001, maxvalue=100)
+        if self.user_input is None:
+            self.user_input = user_input
+            return
         self.update_grid()
-        self.random_pos = random.choice(self.grid)
+
+    def show_info(self):
+        """Display information"""
+        rows, cols = closest_divs(self.max_grid_size)
+        size_info = render_text(f'Grid size: {rows}x{cols}', color=(150, 150, 150))
+        chance_info = render_text(f'Chance: ~ 1 / {self.max_grid_size}', color=(150, 150, 150))
+
+        self.screen.blit(size_info, (650, 0))
+        self.screen.blit(chance_info, (650, 20))
 
     def update_grid(self):
         """Update grid and update man's size."""
@@ -58,8 +84,17 @@ class PercentVisualisation(Game):
         self.grid = [(row * self.man_size + x_offset, col * self.man_size + y_offset)
                      for row in range(rows) for col in range(cols)]
 
-        # print(rows, cols, rows * cols, self.max_grid_size, len(self.grid))
-        # print(self.grid)
+        # setting random pos for yellow guy
+        self.random_pos = random.choice(self.grid)
+
+    def random_value(self):
+        self.user_input = round(random.random(), 3)
+        self.update_grid()
+        pygame.time.wait(100)
+
+    def info_switch(self):
+        self.info = not self.info
+        pygame.time.wait(100)
 
     def main(self):
         """Main state of the program."""
@@ -69,8 +104,7 @@ class PercentVisualisation(Game):
             if event.type == pygame.QUIT:
                 self.running = False
             if event.type == pygame.KEYDOWN:
-                if event.key == pygame.K_SPACE:
-                    self.user_entry()
+                pass
 
         # drawing all blue man images
         for pos in self.grid:
@@ -80,12 +114,19 @@ class PercentVisualisation(Game):
         self.screen.blit(pygame.transform.scale(self.YELLOW_MAN, (self.man_size, self.man_size)), self.random_pos)
         self.screen.blit(pygame.transform.scale(self.LIGHT, (self.man_size, self.man_size)), self.random_pos)
 
+        # display percent value
+        print_text(self.screen, f'{int(self.user_input) if self.user_input >= 1 else self.user_input}%', 0, 0, size=50)
+
+        # show information about grid size and chance
+        if self.info:
+            self.show_info()
+
+        # draw buttons
+        for button in self.buttons:
+            button.draw()
+
 
 if __name__ == '__main__':
     my_game = PercentVisualisation()
     my_game.running = True
     my_game.start()
-    # num = 100
-    # divs = all_divs(num)
-    # print(divs)
-    # print(closest_divs(num))
--- utils.py
+++ utils.py
@@ -1,3 +1,6 @@
+import pygame
+
+
 def all_divs(number):
     """Finding all dividers of the number."""
     dividers = []
@@ -12,3 +15,17 @@ def closest_divs(number):
     dividers = all_divs(number)
     center = dividers[len(dividers)//2]
     return center, int(number / center)
+
+
+def print_text(screen, message, x, y, color=(255, 255, 255), size=20, font=None):
+    text = render_text(message, color, size, font)
+    screen.blit(text, (x, y))
+
+
+def render_text(message, color=(255, 255, 255), size=20, font=None):
+    if font is None:
+        font = pygame.font.SysFont('Arial', size)
+    else:
+        font = pygame.font.Font(font, size)
+    text = font.render(message, True, color)
+    return text
