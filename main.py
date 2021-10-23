142df2645ea6ac55879bee10665a4a091f5defa1
crawlic-stud <nikitosik0726@gmail.com>
2021-10-23 22:56:46 +0300

    GUI 1.0

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import pygame
 from classes import Game, Button
 from utils import closest_divs, print_text, render_text
 from math import sqrt
+pygame.init()
 pygame.font.init()
 
 
@@ -26,16 +27,30 @@ class PercentVisualisation(Game):
         self.man_size = int(sqrt((self.square_width * self.square_height) / self.max_grid_size))
         self.random_pos = (0, 0)
         self.info = False
+        self.options = False
+
+        # 0 - 1st mode
+        # 1 - 2nd mode
+        self.mode = 0
 
         self.buttons = [
-            Button([10, self.height-40, 200, 30], self.screen, 'Change value',
-                   (50, 50, 50), self.BACKGROUND, command=self.user_entry),
-            Button([210, self.height - 40, 200, 30], self.screen, 'Random value',
-                   (50, 50, 50), self.BACKGROUND, command=self.random_value),
-            Button([410, self.height - 40, 200, 30], self.screen, 'Show info',
-                   (50, 50, 50), self.BACKGROUND, command=self.info_switch),
-            Button([610, self.height - 40, 180, 30], self.screen, 'Options',
-                   (50, 50, 50), self.BACKGROUND, command=None),
+            Button([10, self.height-40, 195, 30], self.screen, 'Change value',
+                   (50, 50, 50), (40, 40, 40), command=self.user_entry),
+            Button([205, self.height - 40, 195, 30], self.screen, 'Random value',
+                   (50, 50, 50), (40, 40, 40), command=self.random_value),
+            Button([400, self.height - 40, 195, 30], self.screen, 'Show info',
+                   (50, 50, 50), (40, 40, 40), command=self.info_switch),
+            Button([595, self.height - 40, 195, 30], self.screen, 'Options',
+                   (50, 50, 50), (40, 40, 40), command=self.options_switch),
+        ]
+
+        self.option_buttons = [
+            Button([self.width - 700, self.height - 600, 300, 50], self.screen, 'Mode 1',
+                   (50, 50, 50), (40, 40, 40), command=lambda: self.mode_switch(0)),
+            Button([self.width - 400, self.height - 600, 300, 50], self.screen, 'Mode 2',
+                   (50, 50, 50), (40, 40, 40), command=lambda: self.mode_switch(1)),
+            Button([self.width - 110, self.height - 610, 20, 20], self.screen, 'X',
+                   (50, 50, 50), (40, 40, 40), command=self.options_switch),
         ]
 
     def setup(self):
@@ -60,9 +75,19 @@ class PercentVisualisation(Game):
         rows, cols = closest_divs(self.max_grid_size)
         size_info = render_text(f'Grid size: {rows}x{cols}', color=(150, 150, 150))
         chance_info = render_text(f'Chance: ~ 1 / {self.max_grid_size}', color=(150, 150, 150))
+        mode_info = render_text(f'Mode: {self.mode + 1}', color=(150, 150, 150))
 
         self.screen.blit(size_info, (650, 0))
         self.screen.blit(chance_info, (650, 20))
+        self.screen.blit(mode_info, (650, 40))
+
+    def show_options(self):
+        """Display options."""
+        pygame.draw.rect(self.screen, (40, 40, 40), (self.width - 700, self.height - 600, 600, 400))
+        for button in self.option_buttons:
+            button.radius = 0
+            button.draw()
+        print_text(self.screen, 'Sorry, didn\'t implement that yet :(', self.width - 600, self.height - 400, size=35)
 
     def update_grid(self):
         """Update grid and update man's size."""
@@ -89,11 +114,21 @@ class PercentVisualisation(Game):
 
     def random_value(self):
         self.user_input = round(random.random(), 3)
+        if random.randint(0, 1):
+            self.user_input += random.randint(0, 100)
         self.update_grid()
         pygame.time.wait(100)
 
     def info_switch(self):
         self.info = not self.info
+        pygame.time.wait(100)
+
+    def options_switch(self):
+        self.options = not self.options
+        pygame.time.wait(100)
+
+    def mode_switch(self, mode):
+        self.mode = mode
         pygame.time.wait(100)
 
     def main(self):
@@ -117,16 +152,19 @@ class PercentVisualisation(Game):
         # display percent value
         digits = [num for num in range(1, 101)]
         print_text(self.screen, f'{int(self.user_input) if self.user_input in digits else self.user_input}%',
-                   0, 0, size=50)
-        print_text(self.screen, f'{int(self.user_input) if self.user_input >= 1 else self.user_input}%', 0, 0, size=50)
+                   5, 5, size=50)
+
+        # draw buttons
+        for button in self.buttons:
+            button.radius = 0
+            button.draw()
 
         # show information about grid size and chance
         if self.info:
             self.show_info()
 
-        # draw buttons
-        for button in self.buttons:
-            button.draw()
+        if self.options:
+            self.show_options()
 
 
 if __name__ == '__main__':
